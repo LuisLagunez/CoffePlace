2f96ec1e46c6256dbcf4440e09bfdd2c80db9062
Luis <A20216349@alumnos.uady.mx>
2023-05-21 20:02:10 -0600

    Realicé una validación en el LoginActivity, si los datos de correoValido y contraseniaValida son correctos entonces se procede a realizar el log-in.

--- app/src/main/java/com/example/proyecto/LoginActivity.java
+++ app/src/main/java/com/example/proyecto/LoginActivity.java
@@ -7,24 +7,53 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText et_correo;
+    private EditText et_contrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_correo = (EditText) findViewById(R.id.etcorreologin);
+        et_contrasenia = (EditText) findViewById(R.id.etpasswordlogin);
     }
 
     // Método para acceder al activity de registro.
 
-    public void Registrar(View view) {
-        Intent registrar = new Intent(this, RegistroActivity.class);
-        startActivity(registrar);
+    public void Loguearse(View view) {
+
+        String correoIngresado = et_correo.getText().toString();
+        String correoValido = "luislagrod18@gmail.com";
+        String contraseniaIngresada = et_contrasenia.getText().toString();
+        String contraseniaValida = "ninetails12";
+
+        if(correoIngresado.equals(correoValido) && contraseniaIngresada.equals(contraseniaValida))
+        {
+            Intent prueba = new Intent(this, VistaUnoUsuarioActivity.class);
+            startActivity(prueba);
+        }
+        else if (correoIngresado.equals(correoValido) && !contraseniaIngresada.equals(contraseniaValida))
+        {
+            Toast.makeText(this, "La contraseña ingresada es incorrecta.", Toast.LENGTH_SHORT).show();
+        }
+        else if (correoIngresado.isEmpty() || contraseniaIngresada.isEmpty())
+        {
+            Toast.makeText(this, "Debes rellenar los campos.", Toast.LENGTH_SHORT).show();
+        }
+        else if (!correoIngresado.equals(correoValido))
+        {
+            Toast.makeText(this, "El correo ingresado no existe.", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
-    public void Prueba(View view) {
-        Intent prueba = new Intent(this, VistaUnoUsuarioActivity.class);
-        startActivity(prueba);
+    public void AccederRegistro(View view) {
+        Intent registrar = new Intent(this, RegistroActivity.class);
+        startActivity(registrar);
     }
 }
