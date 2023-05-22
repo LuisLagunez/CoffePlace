1dbe5d352d3622babc81b91bbd9f58c8404d7de1
Luis <A20216349@alumnos.uady.mx>
2023-05-21 22:13:56 -0600

    Se agregó el Shared Preferences al log-in.

--- app/src/main/java/com/example/proyecto/LoginActivity.java
+++ app/src/main/java/com/example/proyecto/LoginActivity.java
@@ -2,28 +2,52 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText et_correoLogin;
     private EditText et_correo;
     private EditText et_contrasenia;
+    private CheckBox cb_guardarCorreo;
+    private SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        et_correoLogin = (EditText) findViewById(R.id.etcorreologin);
         et_correo = (EditText) findViewById(R.id.etcorreologin);
         et_contrasenia = (EditText) findViewById(R.id.etpasswordlogin);
+        cb_guardarCorreo = (CheckBox) findViewById(R.id.cbrecuerdamelogin);
+
+        sharedPreferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        et_correoLogin.setText(sharedPreferences.getString("mail", ""));
     }
 
-    // Método para acceder al activity de registro.
+    // Método para el botón del SharedPreferences.
+
+    public void GuardarCorreo(View view) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (cb_guardarCorreo.isChecked()) {
+            editor.putString("mail", et_correoLogin.getText().toString());
+        } else {
+            editor.remove("mail");
+        }
+        editor.apply();
+    }
+
+    // Método para realizar log-in.
 
     public void Loguearse(View view) {
 
@@ -51,6 +75,8 @@ public class LoginActivity extends AppCompatActivity {
         }
 
     }
+
+    // Método para acceder al activity de registro.
 
     public void AccederRegistro(View view) {
         Intent registrar = new Intent(this, RegistroActivity.class);
