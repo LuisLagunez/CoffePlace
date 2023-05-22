31c115ab679570b113484eca478e589280737888
Luis <A20216349@alumnos.uady.mx>
2023-05-21 19:45:18 -0600

    Implementé validacion en los campos de RegistroActivity, al dar clic en el botón de Registrar este te devuelve al activity_login.

--- app/src/main/java/com/example/proyecto/RegistroActivity.java
+++ app/src/main/java/com/example/proyecto/RegistroActivity.java
@@ -1,17 +1,14 @@
 package com.example.proyecto;
 
-import static com.example.proyecto.R.id.bregistrologin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class RegistroActivity extends AppCompatActivity{
@@ -20,6 +17,10 @@ public class RegistroActivity extends AppCompatActivity{
     private EditText et_correo;
     private EditText et_numero;
     private EditText et_contrasenia;
+    private EditText et_confirmarContrasenia;
+    private RadioButton rb_hombre;
+    private RadioButton rb_mujer;
+    private CheckBox cb_terminos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,63 +31,30 @@ public class RegistroActivity extends AppCompatActivity{
         et_correo = (EditText) findViewById(R.id.etemailregistro);
         et_numero = (EditText) findViewById(R.id.etptelefonoregistro);
         et_contrasenia = (EditText) findViewById(R.id.etcontrasenaregistro);
+        et_confirmarContrasenia = (EditText) findViewById(R.id.etconfirmarcontrasenaregistro);
+        rb_hombre = (RadioButton) findViewById(R.id.rbhombrelogin);
+        rb_mujer = (RadioButton) findViewById(R.id.rbmujerlogin);
+        cb_terminos = (CheckBox) findViewById(R.id.cbterminosregistro);
     }
 
     // Método para dar de alta a usuarios
 
-    public void RegistrarUsuario(View view) {
-        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "Usuarios", null, 1);
-        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
+    public void Registrar(View view) {
 
         String nombre = et_nombre.getText().toString();
-        String correo = et_nombre.getText().toString();
-        String numero = et_nombre.getText().toString();
-        String contrasenia = et_nombre.getText().toString();
+        String correo = et_correo.getText().toString();
+        String numero = et_numero.getText().toString();
+        String contrasenia = et_contrasenia.getText().toString();
+        String confirmarContrasenia = et_confirmarContrasenia.getText().toString();
 
-        if (!nombre.isEmpty() && !correo.isEmpty() && !numero.isEmpty() && !contrasenia.isEmpty()) {
-            ContentValues registro = new ContentValues();
-            registro.put("Nombre", nombre);
-            registro.put("Correo", correo);
-            registro.put("Numero", numero);
-            registro.put("Contrasenia", contrasenia);
-
-            BaseDeDatos.insert("usuarios", null, registro);
-
-            BaseDeDatos.close();
-            et_nombre.setText("");
-            et_correo.setText("");
-            et_numero.setText("");
-            et_contrasenia.setText("");
-
-            Toast.makeText(this, "Registro Éxitoso.", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this, "Debes llenar todos los campos.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    // Método para consultar usuarios.
-
-    public void BuscarUsuarios(View view) {
-        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "Usuarios", null, 1);
-        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
-
-        String correoElectronico = et_correo.getText().toString();
-
-        if (!correoElectronico.isEmpty()) {
-            Cursor fila = BaseDeDatos.rawQuery
-                    ("select nombre, numero, contrasenia, from Usuarios where correoElectronico = " + correoElectronico, null);
-
-            if (fila.moveToFirst()) {
-                et_nombre.setText(fila.getString(0));
-                et_numero.setText(fila.getString(1));
-                et_contrasenia.setText(fila.getString(2));
-                BaseDeDatos.close();
-            }else {
-                Toast.makeText(this, "No existe el correo electrónico.", Toast.LENGTH_SHORT).show();
-                BaseDeDatos.close();
-            }
-        }else {
-            Toast.makeText(this, "Introduce el correo electrónico a buscar.", Toast.LENGTH_SHORT).show();
+        if (!nombre.isEmpty() && !correo.isEmpty() && !numero.isEmpty() && !contrasenia.isEmpty()
+        && !confirmarContrasenia.isEmpty() && (rb_hombre.isChecked() || rb_mujer.isChecked()) && cb_terminos.isChecked())
+        {
+            Toast.makeText(this, "Registro Éxitoso", Toast.LENGTH_SHORT).show();
+            Intent anterior = new Intent(this, LoginActivity.class);
+            startActivity(anterior);
+        } else{
+            Toast.makeText(this, "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
         }
     }
 
