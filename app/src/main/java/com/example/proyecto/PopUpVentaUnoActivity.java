7693c5467cdeffef429a1c56800a0019e4bcdbdd
Luis <A20216349@alumnos.uady.mx>
2023-05-23 02:18:10 -0600

    Fin de proyecto.

--- app/src/main/java/com/example/proyecto/PopUpVentaUnoActivity.java
+++ app/src/main/java/com/example/proyecto/PopUpVentaUnoActivity.java
@@ -1,21 +1,25 @@
 package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
 public class PopUpVentaUnoActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_up_venta_uno);
+
 
         DisplayMetrics medidaVentana = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(medidaVentana);
@@ -24,6 +28,7 @@ public class PopUpVentaUnoActivity extends AppCompatActivity {
         int alto = medidaVentana.heightPixels;
 
         getWindow().setLayout((int)(ancho * 0.85), (int)(alto * 0.5));
+
     }
 
     // Método para confirmar pedido.
